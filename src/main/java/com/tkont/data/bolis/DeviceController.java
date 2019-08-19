f891eee4ca37d592eb068b288371faff18079c28
technokon <iakoupov@gmail.com>
2019-08-19 12:01:59 -0400

    added sse emitter

--- src/main/java/com/tkont/data/bolis/DeviceController.java
+++ src/main/java/com/tkont/data/bolis/DeviceController.java
@@ -2,8 +2,13 @@ package com.tkont.data.bolis;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by iakoupov on 2019-08-19.
@@ -21,5 +26,31 @@ public class DeviceController {
         data.setOperationCapacity(30);
         data.setTemp(75);
         return data;
+    }
+
+    @RequestMapping("/device-data-live")
+    public SseEmitter requestLiveData() {
+        Timer timer = new Timer();
+        Random random = new Random();
+        SseEmitter emitter = new SseEmitter();
+        TimerTask dispatcher = new TimerTask() {
+            @Override
+            public void run() {
+                DeviceData data = new DeviceData();
+                data.setItemProcessingId(random.nextInt(1000) + 1000);
+                data.setName("Turbine shaft x54");
+                data.setStart(new Date());
+                data.setOperationCapacity(random.nextInt(100));
+                data.setTemp(random.nextInt(500) + 50);
+                try {
+                    emitter.send(data);
+                } catch (IOException exception) {
+                    System.out.println(exception);
+                    emitter.completeWithError(exception);
+                }
+            }
+        };
+        timer.scheduleAtFixedRate(dispatcher, 0, 300);
+        return emitter;
     }
 }
